0679181038301ea71717037cfc90d2cd46df9771
Marco <202216577@alu.comillas.edu>
2026-02-08 23:05:55 +0100

    Nuevos cambios en el controlador

--- src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/controladores/CarritoControlador.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/controladores/CarritoControlador.java
@@ -8,38 +8,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/api/carrito")
 public class CarritoControlador {
     private final Map<Integer, Carrito> carritos = new HashMap<>();
 
-    @PostMapping("/api/carrito")
+    @PostMapping
     public ResponseEntity<Carrito> crearCarrito(@RequestBody Carrito nuevoCarrito) {
         carritos.put(Carrito.getIdCarrito(), nuevoCarrito);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoCarrito);
     }
 
-    @GetMapping("/api/carrito")
+    @GetMapping
     public List<Carrito> getCarritos() {
         return new ArrayList<>(carritos.values());
     }
 
-    @GetMapping("/api/carrito/{idCarrito}")
+    @GetMapping("/{idCarrito}")
     public ResponseEntity<Carrito> getCarrito(@PathVariable int idCarrito) {
         Carrito c = carritos.get(idCarrito);
         return (c != null) ? ResponseEntity.ok(c) : ResponseEntity.notFound().build();
     }
 
-    @DeleteMapping("/api/carrito/{idCarrito}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void borrarCarrito(@PathVariable int idCarrito) {
-        carritos.remove(idCarrito);
-    }
-
-    @PutMapping("/api/carrito/{idCarrito}")
+    @PutMapping("/{idCarrito}")
     public ResponseEntity<Carrito> modificaCarrito(@PathVariable Integer idCarrito, @RequestBody Carrito carrito) {
         if (!carritos.containsKey(idCarrito)) {
             return ResponseEntity.notFound().build();
         }
         carritos.put(idCarrito, carrito);
         return ResponseEntity.ok(carrito);
+    }
+
+    @DeleteMapping("/{idCarrito}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void borrarCarrito(@PathVariable int idCarrito) {
+        carritos.remove(idCarrito);
     }
 }
